9849369285958995d34ec7b5a7eaafc07d16461d
Prateek Donni <prateek.donni@am.sony.com>
2014-03-21 01:03:39 -0700

    Completed csv to json

--- nodefileread.js
+++ nodefileread.js
@@ -1,8 +1,16 @@
 var fs = require('fs');
+var jf = require('jsonfile')
+
+
 fs.readFile('/Users/pdonni/Documents/Github/learnNode/file.txt',{encoding: 'utf8'}, function (err, data) {
   if (err) throw err;
   //console.log(data);
 });
+var jsonObject=[];
+
+
+
+
 
 var Lazy = require('lazy'),
 
@@ -11,14 +19,24 @@ lazy = new Lazy(fs.createReadStream('/Users/pdonni/Documents/Github/learnNode/fi
     .map(String)
     .map(function(lineData){
      	var fields = lineData.trim().split(',', 4);
-     	console.log(fields[0]);
-     	return {
-     		parsed: {
-     			col1: fields[0],
-     			col2: fields[1],
-     			col3: fields[2],
-     			col4: fields[3]
-     		}
-     	}
+     	var jsoninput = {
+     		col1: fields[0],
+     		col2: fields[1],
+     		col3: fields[2],
+     		col4: fields[3]
+     	};
+     	
+     	jsonObject.push(jsoninput);
+     	console.log(jsonObject);
+     	var file = '/Users/pdonni/Documents/Github/learnNode/data.json';
+		jf.writeFile(file, { data : jsonObject }, function(err) {
+  
+		})
+
      })
  
+
+
+
+
+ //console.log(jsonObject);
