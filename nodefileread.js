cb13d516fe05ce31ac77e3332e5593aa77d3126b
Prateek Donni <prateek.donni@am.sony.com>
2014-03-21 00:48:52 -0700

    Initial try with lazy

--- nodefileread.js
+++ nodefileread.js
@@ -1,5 +1,24 @@
 var fs = require('fs');
 fs.readFile('/Users/pdonni/Documents/Github/learnNode/file.txt',{encoding: 'utf8'}, function (err, data) {
   if (err) throw err;
-  console.log(data);
+  //console.log(data);
 });
+
+var Lazy = require('lazy'),
+
+lazy = new Lazy(fs.createReadStream('/Users/pdonni/Documents/Github/learnNode/file.txt'))
+    .lines
+    .map(String)
+    .map(function(lineData){
+     	var fields = lineData.trim().split(',', 4);
+     	console.log(fields[0]);
+     	return {
+     		parsed: {
+     			col1: fields[0],
+     			col2: fields[1],
+     			col3: fields[2],
+     			col4: fields[3]
+     		}
+     	}
+     })
+ 
