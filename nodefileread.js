18dbae4846b45eaa5f79509530b5000716f04dc2
Prateek Donni <prateek.donni@am.sony.com>
2014-03-23 17:35:19 -0700

    Adding Highcharts

--- nodefileread.js
+++ nodefileread.js
@@ -1,42 +1,55 @@
 var fs = require('fs');
 var jf = require('jsonfile')
-
-
-fs.readFile('/Users/pdonni/Documents/Github/learnNode/file.txt',{encoding: 'utf8'}, function (err, data) {
-  if (err) throw err;
-  //console.log(data);
-});
+var val = require('validator')
 var jsonObject=[];
+var lineNumber=0;
+var columns=[];
 
-
+// object for creating HighChart bar graph
+var chartObject = {
+                    chart: {
+                        type: "column"
+                    },
+                    title: {
+                        text: "CPU Utilization"
+                    },
+                    xAxis: {
+                        categories: []
+                           },
+                    yAxis: {
+                        title: {
+                            text: "Percentage"
+                        }
+                    },
+                    series: [{
+                        name: "CPU Percentage",
+                        data: []
+                    }]        };
 
 
 
 var Lazy = require('lazy'),
-
-lazy = new Lazy(fs.createReadStream('/Users/pdonni/Documents/Github/learnNode/file.txt'))
+lazy = new Lazy(fs.createReadStream('/Users/pdonni/Documents/Github/learnNode/Book2.csv'))
     .lines
     .map(String)
     .map(function(lineData){
-     	var fields = lineData.trim().split(',', 4);
-     	var jsoninput = {
-     		col1: fields[0],
-     		col2: fields[1],
-     		col3: fields[2],
-     		col4: fields[3]
-     	};
-     	
-     	jsonObject.push(jsoninput);
-     	console.log(jsonObject);
-     	var file = '/Users/pdonni/Documents/Github/learnNode/data.json';
-		jf.writeFile(file, { data : jsonObject }, function(err) {
-  
-		})
-
-     })
- 
-
-
-
-
- //console.log(jsonObject);
+        var fields = lineData.trim().split(',', 11);
+        if(lineNumber == 0)
+        {
+            columns.push(fields[3]);
+            columns.push(fields[10]);     
+        } else {
+            // var jsoninput = {
+            //     MEM : fields[3],
+            //     CPU : fields[10]
+            // };
+            // jsonObject.push(jsoninput);
+            // 
+            chartObject.xAxis.categories.push(fields[10]);  
+            chartObject.series[0].data.push(val.toFloat(fields[3]));  
+            var file = '/Users/pdonni/Documents/Github/learnNode/data.json';
+            jf.writeFile(file, chartObject);
+        }
+        lineNumber = lineNumber + 1;
+        
+    });
